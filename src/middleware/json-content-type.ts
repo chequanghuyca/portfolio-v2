cb378f974b7e24694fe2d53a2120a769d0da20bd
Che Quang Huy <chequanghuybtt@gmail.com>
2025-09-19 16:55:32 +0700

    chore: update

--- src/middleware/json-content-type.ts
+++ src/middleware/json-content-type.ts
@@ -2,12 +2,11 @@ import type { RequestConfig } from './http-client';
 
 export default () => {
 	return (config: RequestConfig) => {
-		const apiKey = (import.meta as any).env?.VITE_API_KEY as string | undefined;
 		config.headers = {
 			...(config.headers || {}),
 			'Content-Type': 'application/json; charset=utf-8',
 			Accept: 'application/json',
-			...(apiKey ? { 'x-api-key': apiKey } : {}),
+			'x-api-key': import.meta.env.VITE_API_KEY as string | undefined,
 		};
 
 		return config;
